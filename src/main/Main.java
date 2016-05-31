bce0fcab6f9685e177f9526010e2e026a89d2c65
MHersisia <mikehersisia@gmail.com>
2016-05-31 12:20:18 +0200

    Changes s to text

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		write("String");
 	}
-	public void write(String s){
-		System.out.println(s);
+	public static void write(String text){
+		System.out.println(text);
 	}
 }
